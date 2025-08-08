5447ec187ba5e3e781af45ae6ba894017544c352
Mariya Gorbaneva <mariya_alexa@mail.ru>
2025-08-08 11:25:02 +0300

    isort все исправил

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
+                                    create_async_engine)
 from sqlalchemy.orm import declarative_base
-
 
 DB_URL = "sqlite+aiosqlite:///./app.db"
 engine = create_async_engine(DB_URL, echo=True)
